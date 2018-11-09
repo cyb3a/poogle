ecfba9f4223bae9c9a0356d43af11d7f6d72e550
tabea <tabea@mbr-targeting.com>
2018-11-09 13:26:55 +0100

    Normalize Text and Stem

--- data_processing.py
+++ data_processing.py
@@ -3,8 +3,12 @@
 import json
 import nltk
 from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer  # Support for German language stemming
 
 nltk.download('punkt')
+stopWords = set(stopwords.words('german'))
+stemmer = SnowballStemmer("german")
 
 # Read the Json file
 json_data = 'testing.json'  # change here
@@ -16,6 +20,14 @@ words = []
 for report in data:
     words.append((report['URL'], word_tokenize(report['Title']+" "+report['Content'])))
 
-## Remove Stopwords
+# Look for collocations with WHATEVER.collocations() -> common phrases, 'Eigennamen'
+collocs = []
+
+# Normalize & Stem
+normed = []
+for tokens in words:
+    normed.append((tokens[0], [stemmer.stem(w.lower()) for w in tokens[1]]))  # lowercase
+
+# Remove Stopwords & Punctuation
 
 ## Count Tokens
