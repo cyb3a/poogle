6b974aff90e53ca6a4e86facabcab844608f6a48
tabea <tabea@mbr-targeting.com>
2018-11-09 13:01:34 +0100

    Tokenize title and content of each json object

--- data_processing.py
+++ data_processing.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import json
+import nltk
+from nltk import word_tokenize
+
+nltk.download('punkt')
 
 # Read the Json file
 json_data = 'testing.json'  # change here
 raw = open(json_data)
 data = json.load(raw)
 
-
-
-## Tokenize Content
+# Tokenize
+words = []
+for report in data:
+    words.append((report['URL'], word_tokenize(report['Title']+report['Content'])))
 
 ## Remove Stopwords
 
