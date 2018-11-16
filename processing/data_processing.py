b43151853b9d3f1699863e3e2a3b719a450117ec
tabea <tabea@mbr-targeting.com>
2018-11-16 13:58:05 +0100

    Upload dummy data to database reports table

--- processing/data_processing.py
+++ processing/data_processing.py
@@ -5,6 +5,9 @@ import nltk
 from nltk import word_tokenize
 from nltk.stem.snowball import SnowballStemmer  # Support for German language stemming
 
+from processing.secrets import pwd, usr, hst, dab
+import pymysql
+
 nltk.download('punkt')
 stemmer = SnowballStemmer("german")
 
@@ -17,24 +20,42 @@ with open(stopwords_path) as f:
 
 # Read the Json file
 json_data = 'processing/testing.json'  # change here
-raw = open(json_data)
-data = json.load(raw)
+with open(json_data) as raw:
+    data = json.load(raw)
 
 # Tokenize & Lowercase & Remove Stopwords
-words = []
+words = {}
 for report in data:
     x = word_tokenize(report['Title']+' '+report['Content'])
     y = [tok.lower() for tok in x if tok.lower() not in stopwords]
-    words.append((report['URL'], y))
+    words[report['URL']] = y
 
 # Look for collocations with WHATEVER.collocations() -> common phrases, 'Eigennamen'
-collocs = []
+# collocs = {}
 
 # Stem
-normed = []
+normed = {}
 
-for tokens in words:
-    normed.append((tokens[0], [stemmer.stem(w) for w in tokens[1]]))  # lowercase
+for url, tokens in words.items():
+    normed[url] = [stemmer.stem(w) for w in tokens]
 
-print(normed)
-## Count Tokens
+## Count Tokens TODO
+
+# Database
+pymysql.install_as_MySQLdb()
+import MySQLdb
+db = pymysql.connect(host=hst,
+                       user=usr,
+                       passwd=pwd,
+                       db=dab)
+
+try:
+    cur = db.cursor()
+    cur.executemany("""
+        INSERT INTO reports (URL, Title, Locations, Dates, Content, CreatedAt)
+        VALUES (%(URL)s, %(Title)s, %(Locations)s, %(Dates)s, %(Content)s, %(CreatedAt)s)""", data)
+    db.commit()
+finally:
+    cur.close()
+
+db.close()
